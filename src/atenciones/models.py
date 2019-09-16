46b163fca1a683e51c7c27e1c6a884f429787f77
Javier <jquappev@gmail.com>
2019-09-16 17:03:19 -0300

    Delete models.py

--- src/atenciones/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Atencion(models.Model):
-	id_vehiculo	= models.ForeignKey('vehiculos.Vehiculo'),
-	fecha_entrada	= models.DateTimeField(),
-	fecha_salida	= models.DateTimeField(blank = True)
