a9045c5d4a6602c23a3db6dc642fd2c8383b705f
Javier <jquappev@gmail.com>
2019-09-16 17:03:11 -0300

    Delete models.py

--- src/vehiculos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Vehiculo(models.Model):
-	id_cliente	= models.ForeignKey('clientes.Cliente'),
-	patente		= models.CharField(max_length = 6),
-	marca		= models.CharField(max_length = 40),
-	modelo		= models.CharField(max_length = 40),
-	kilometraje	= models.PositiveIntegerField()
