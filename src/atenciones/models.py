7e31867679e9f5d126d20ce8dd3cefa987b652fc
Javier <jquappev@gmail.com>
2019-09-16 16:56:58 -0300

    Update models.py

--- src/atenciones/models.py
+++ src/atenciones/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Atencion(models.Model):
 	id_vehiculo	= models.ForeignKey('vehiculos.Vehiculo'),
 	fecha_entrada	= models.DateTimeField(),
-	fecha_salida	= models.DateTimeField()
+	fecha_salida	= models.DateTimeField(blank = True)
