f3418184f58e7aa47340c3eb586a4925a4b53698
Javier <jquappev@gmail.com>
2019-09-16 16:54:18 -0300

    Create models.py

---
+++ src/atenciones/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Atencion(models.Model):
+	id_vehiculo	= models.ForeignKey('vehiculos.Vehiculo'),
+	fecha_entrada	= models.DateTimeField(),
+	fecha_salida	= models.DateTimeField()
